a534860a8d08275299d2d0fbe920a0f7804f8628
zhanghaixu <30704448724@qq.com>
2023-09-10 10:04:23 +0800

    fix(config):解决mybatis分页查total显示异常bug

---
+++ src/main/kotlin/com/example/server/config/MybatisPlusConfig.kt
@@ -0,0 +1,17 @@
+package com.example.server.config
+
+import com.baomidou.mybatisplus.annotation.DbType
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class MybatisPlusConfig {
+    @Bean
+    public fun mybatisPlusInterceptor() : MybatisPlusInterceptor {
+        val interceptor = MybatisPlusInterceptor()
+        interceptor.addInnerInterceptor(PaginationInnerInterceptor(DbType.H2));
+        return interceptor;
+    }
+}
